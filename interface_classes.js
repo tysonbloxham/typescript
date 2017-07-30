8e8127028473320e87011024e6b2d487a62ce851
Tyson Bloxham <tysonsbloxham@gmail.com>
2017-07-30 16:23:40 -0600

    Initial commit

---
+++ interface_classes.js
@@ -0,0 +1,29 @@
+// Loosely connected interface with Class
+var Admin = (function () {
+    function Admin(email) {
+        this.email = email;
+        this.role = 'Admin';
+    }
+    return Admin;
+}());
+function profile(user) {
+    return "Welcome, " + user.email;
+}
+var joe = new Admin('joe@example.com');
+console.log(joe.role);
+var Post = (function () {
+    function Post(post) {
+        this.title = post.title;
+        this.body = post.body;
+    }
+    Post.prototype.printPost = function () {
+        console.log(this.title);
+        console.log(this.body);
+    };
+    return Post;
+}());
+var post = new Post({ title: "My Great Title", body: "Some content" });
+console.log(post.title);
+console.log(post.body);
+post.printPost();
+//# sourceMappingURL=interface_classes.js.map
---
+++ interface_functions.js
@@ -0,0 +1,7 @@
+var myInvoice;
+myInvoice = function (n, t) {
+    console.log(n);
+    console.log(t);
+};
+myInvoice('Google', 500);
+//# sourceMappingURL=interface_functions.js.map
