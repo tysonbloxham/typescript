0ec9d9032c9bd345c53db2aa5430d00e119059f8
Tyson Bloxham <tysonsbloxham@gmail.com>
2018-03-05 17:12:16 -0700

    Basic hello world

---
+++ helloWorld.js
@@ -0,0 +1,8 @@
+var HelloAngularComponent = /** @class */ (function () {
+    function HelloAngularComponent() {
+    }
+    HelloAngularComponent.prototype.constuctor = function () {
+        this.greeting = 'Hello Angular 4';
+    };
+    return HelloAngularComponent;
+}());
---
+++ helloWorld.ts
@@ -0,0 +1,6 @@
+class HelloAngularComponent {
+  greeting: string;
+  constuctor() {
+    this.greeting = 'Hello Angular 4';
+  }
+}
